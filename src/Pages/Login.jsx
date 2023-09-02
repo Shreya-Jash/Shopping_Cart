08a5d805ac1c20cb41054dc5579c9e448cfe491a
Shreya Jash <shreyajash98@gmail.com>
2023-09-02 20:44:37 +0530

    style: login ui modified

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -12,74 +12,76 @@ function Login() {
   const { logIn, googleSignIn } = useUserAuth();
   const navigate = useNavigate();
 
-  const handleSubmit =async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
     try {
-      await logIn(email,password);
-      navigate("/")
+      await logIn(email, password);
+      navigate("/");
     } catch (err) {
       setError(err.message);
     }
-  }
+  };
 
-  const handleGoogleSignIn= async(e) => {
+  const handleGoogleSignIn = async (e) => {
     e.preventDefault();
     try {
       await googleSignIn();
-      navigate('/')
+      navigate("/");
     } catch (err) {
       setError(err.message);
     }
-  }
-  // const [user,setUser]=useState({
-  //   email:"",
-  //   password:""
-  // })
-
-  // function handleChange(e)
-  // {
-  //   const {name,value}=e.target
-  //   console.log(e.target)
-  //   setUser({
-  //     ...user,
-  //     [name]:value
-  //   })
-  // }
+  };
 
   return (
-    <div>
-      <p>Welcome Back!</p>
-      {error && <div role="alert" className="bg-red-500 text-white font-bold rounded-t px-4 py-2">{error}</div>}
-      <form className="login_page" autoComplete="off" onSubmit={handleSubmit}>
-        <div className="login_text">Your Email</div>
-        <input
-          type="text"
-          className="login_input"
-          name="email"
-          // value={user.email}
-          // onChange={handleChange}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <div className="login_text">Password</div>
-        <input
-          type="password"
-          className="login_input"
-          autoComplete="new-password"
-          name="password"
-          // value={user.password}
-          // onChange={handleChange}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button className="login_button">Login</button>
-        <div>
-          <GoogleButton onClick={handleGoogleSignIn}/>
+    <div className="login flex flex-col rounded-lg shadow-2xl shadow-stone-700 absolute w-80 xs:w-[450px] h-[450px]">
+      <p className="text-xl text-center font-bold text-[bisque]">
+        Login to your account!
+      </p>
+      {error && (
+        <div
+          role="alert"
+          className="bg-red-500 text-white font-bold rounded-t px-4 py-2"
+        >
+          {error}
         </div>
+      )}
+      <form
+        className="login_page flex justify-center"
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex-col w-72 xs:w-96">
+          <div className="login_text">Your Email</div>
+          <input
+            type="text"
+            className="login_input w-72 xs:w-96 "
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <p>Don't have account?</p>
-        <NavLink to="/Register">Register Now</NavLink>
+          <div className="login_text">Password</div>
+          <input
+            type="password"
+            className="login_input w-72 xs:w-96"
+            autoComplete="new-password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="login_button w-1/5 h-6 text-sm ">Login</button>
+          <div className="h-px bg-[bisque] mt-4 mb-4"></div>
+          <div>
+            <GoogleButton onClick={handleGoogleSignIn} />
+          </div>
+
+          <p className="text-center pt-5 text-[bisque]">
+            Don't have account?{" "}
+            <NavLink to="/Register" className="text-[#f7b769]">
+              Register Now
+            </NavLink>
+          </p>
+        </div>
       </form>
     </div>
   );
