3f931cd12e30c480c90e19f64f67626e9363c3d7
Shreya Jash <shreyajash98@gmail.com>
2023-09-02 21:28:32 +0530

    style: product login button style updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,7 +13,7 @@ export default function Products({item ,handleClick}) {
       <p className="item-price">Price- ₹ {item.price}</p>
       <p className="item-quantity">Quantity per pack-{item.quantity}</p>
       </div>
-      <button className="productButton" onClick={() =>{ handleClick(item)}}>
+      <button className="productButton bg-[#158272d0] w-28 text-[bisque] rounded-lg mb-2" onClick={() =>{ handleClick(item)}}>
         {console.log(handleClick)}
         Add item
       </button>
